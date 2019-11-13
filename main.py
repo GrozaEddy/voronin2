1d592b0af76af9fb2fc638a27e6442ffec39fd5a
Эдуард <evoronin2002@yandex.ru>
2019-11-13 21:26:17 +0300

    Fist commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from random import choice
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(self)
+        self.pushButton.clicked.connect(self.run)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_rect(qp)
+        qp.end()
+
+    def run(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        r = choice(list(range(50, 300)))
+        qp.drawEllipse(300, 300, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
